d3a38344e4619aba4e1bf2f88108dca05ccb0ee1
Mahbub Alam <mdmahabubalom611762@gmail.com>
2025-10-15 23:18:39 +0600

    add new page

---
+++ lib/sereens/new_testpaje.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewTestpaje extends StatelessWidget {
+  const NewTestpaje({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Enter Your git "),
+      ),
+    );
+  }
+}
+
